65899599df1ac9468cbbb073b10d558d457ea0a2
Andrew <ad13.works@gmail.com>
2026-04-29 01:14:31 +0200

    initial layout configuration created

---
+++ README.md
@@ -0,0 +1,6 @@
+Monorepo for fe and be.
+
+Be cloned from https://github.com/WebDev-WBSCodingSchool/events-api.git
+
+## To clone
+git clone --recurse-submodules <repo-url>
---
+++ fe/src/App.jsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="text-center space-y-2">
+        <h1 className="text-4xl text-vd-accent2">Eventify</h1>
+      </div>
+    </div>
+  )
+}
+
+export default App
